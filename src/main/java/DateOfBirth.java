2648257ee9632587891e32faee6019bd429b1bee
ZsoltiDe <deak1020@gmail.com>
2022-05-21 19:11:17 +0200

    DateOfBirth

--- src/main/java/DateOfBirth.java
+++ src/main/java/DateOfBirth.java
@@ -1,8 +1,43 @@
 import javafx.beans.property.IntegerProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class DateOfBirth {
+    @Id
+    @GeneratedValue
     private int yearOfBirth;
     private int monthOfBirth;
     private int dayOfBirth;
 
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public int getMonthOfBirth() {
+        return monthOfBirth;
+    }
+
+    public void setMonthOfBirth(int monthOfBirth) {
+        this.monthOfBirth = monthOfBirth;
+    }
+
+    public int getDayOfBirth() {
+        return dayOfBirth;
+    }
+
+    public void setDayOfBirth(int dayOfBirth) {
+        this.dayOfBirth = dayOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return yearOfBirth +"."+monthOfBirth +"."+dayOfBirth ;
+    }
 }
