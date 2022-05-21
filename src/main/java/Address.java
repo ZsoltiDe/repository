66775e90fbd517d2b8136b43bce2ce6f9b863986
ZsoltiDe <deak1020@gmail.com>
2022-05-21 19:37:55 +0200

    Address

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,2 +1,53 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Address {
+    @GeneratedValue
+    @Id
+    private int id;
+    private int zipCode;
+    private String country;
+    private String city;
+    private int houseNumber;
+    private String street;
+
+    public int getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(int zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    @Override
+    public String toString() {
+        return zipCode+", "+country+", "+city+", "+street+" "+houseNumber;
+
+    }
 }
