6028217bbc47a1fb09baaaf4f9375fd168e6b4e8
ZsoltiDe <deak1020@gmail.com>
2022-05-24 13:27:33 +0200

    EmployeeDAO

---
+++ src/main/java/EmployeeDAO.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface EmployeeDAO extends AutoCloseable {
+    public void saveEmployee(Employee employee);
+    public void deleteEmployee(Employee employee);
+    public void updateEmployee(Employee employee);
+    public List<Employee> getEmployee();
+    public void close();
+
+}
